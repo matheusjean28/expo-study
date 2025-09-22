8e4fdc208783150d2955d0284fe34623246510ec
matheusjean28 <matheusjean28@gmail.com>
2025-09-22 17:18:24 -0300

    send whole files

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen name="index" options={{title:"Home"} }/>
+    <Stack.Screen name="About" options={{title:"About"} }/>
+  </Stack>;
+}
---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>teste</Text>
+    </View>
+  );
+  
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
